3d5f134cefdccd155db1d91252fe1fd7b7c4e600
shagina123 <shagina123@gmail.com>
2020-11-05 11:18:54 +0530

    Delete array_utils.h

--- hc/array_utils.h
+++
@@ -1,7 +0,0 @@
-//array_utils.h
-
-int contains(const int *arr,int size , int k);
-int containsWithin(const int *arr,int size , int k,int i , int j);
-int *paddedCopy(const int *arr,int oleSize , int newSize);
-void reverse(int *arr,int size);
-int *reverseCopy(const int *arr,int size);
